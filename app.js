694f8a0ed474b52a5bfeb1112e2ba2deefca3434
EC2 Default User <ec2-user@ip-172-31-31-159.us-west-1.compute.internal>
2020-12-03 21:40:21 +0000

    Unfinished

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function(req, res) {
+    
+    res.send("it works!");
+    
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    
+    console.log("Express server is runnning");
+    
+});
