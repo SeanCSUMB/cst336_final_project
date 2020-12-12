d4168f67784d4516719c5470ccd57d196dfafc63
EC2 Default User <ec2-user@ip-172-31-31-159.us-west-1.compute.internal>
2020-12-12 06:53:13 +0000

    Unfinished

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const pool = require("./dbPool.js");
 const fetch = require("node-fetch");
 const session = require('express-session');
 const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const mysql = require('mysql');
 
 app.set("view engine", "ejs");
@@ -15,71 +16,319 @@ app.use(session({
     saveUninitialized: true
 }));
 
-let productArray = [];
+app.use(express.urlencoded({extended: true}));
 
 //routes
-app.get("/", async function(req, res) {
-  
-    res.render("index");
+app.get("/", isAuthenticated, async function(req, res) {
+    let welcomeName;
+    res.render("index", {"welcomeName": req.session.userLogged});
 });
 
-//TODO:
-//This area of the reviews should be mostly fine, but at the moment all that the reviews page displays is the title of the item.
+//starting server
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Express server is running...");
+    
+    
+});
+
+//////////////// Reviews code //////////////////
+
+//Currently not completely functional.
+app.get("/allReviews", isAuthenticated, function(req, res) {
+  
+  let sql = "SELECT DISTINCT products_ASIN FROM reviews";
+
+  pool.query(sql, function(err, rows, fields) {
+  
+    if (err) throw err;
+    console.log(rows);
+    if (rows == "") {
+    
+      //Some other value may work.
+      res.render("allReviews", {"rows" : rows});
+    
+    }
+  
+    else {
+    
+      res.render("allReviews", {"rows" : rows});
+    
+    }
+  
+  });
+  
+});
+
+//I am unsure whethere we need a whole route for this.
+app.get("/helpful", function(req, res) {
+  
+  console.log("ID is " + req.param('id'));
+  console.log("ASIN is " + req.param('asin'));
+  let id = "";
+  if (req.param('id')) {
+    
+    id = req.param('id');
+    
+  }
+  
+  if (id == "") {
+     
+      //Don't actually return, the page will be stuck in limbo. Just render the previous page.
+      return;
+     
+  }
+  
+  let asin = "";
+  if (req.param('asin')) {
+    
+    id = req.param('asin');
+    
+  }
+  
+  if (asin == "") {
+     
+      //Don't actually return, the page will be stuck in limbo. Just render the previous page.
+      return;
+     
+  }
+  
+  if (typeof(req.session.userLogged) != "undefined") {
+    
+    let sql = "UPDATE reviews SET helpfulVotes = helpfulVotes + 1 WHERE reviews.idreviews = " + id;
+    pool.query(sql, function(err) {
+    
+      if (err) throw err;
+    
+    });
+    
+    res.redirect("/review?asin=" + asin);
+  
+    //This code should be removed before submission, it is only here for posterity and in case something buggy happens.
+    /*sql = "SELECT * FROM reviews WHERE reviews.products_ASIN = " + asin;
+    pool.query(sql, function(err, rows, fields) {
+    
+      if (err) throw err;
+      console.log(rows);
+      if (rows == "") {
+      
+        res.render("review", {"rows" : "It looks like there are no reviews for this product yet.", "id" : id, "asin" : asin});
+      
+      }
+    
+      else {
+      
+        res.render("review", {"rows" : rows, "id" : id, "asin" : asin});
+      
+      }
+    
+    });*/
+  
+  }
+  
+  else {
+    
+    res.render("login");
+    
+  }
+  
+});
+
+//I am unsure whethere we need a whole route for this.
+app.get("/unhelpful", function(req, res) {
+  
+  console.log("ID is " + req.param('id'));
+  console.log("ASIN is " + req.param('asin'));
+  let id = "";
+  if (req.param('id')) {
+    
+    id = req.param('id');
+    
+  }
+  
+  if (id == "") {
+     
+      //Don't actually return, the page will be stuck in limbo. Just render the previous page.
+      return;
+     
+  }
+  
+  let asin = "";
+  if (req.param('asin')) {
+    
+    id = req.param('asin');
+    
+  }
+  
+  if (asin == "") {
+     
+      //Don't actually return, the page will be stuck in limbo. Just render the previous page.
+      return;
+     
+  }
+  
+  if (typeof(req.session.userLogged) != "undefined") {
+    
+    let sql = "UPDATE reviews SET unhelpfulVotes = unhelpfulVotes + 1 WHERE reviews.idreviews = " + id;
+    pool.query(sql, function(err) {
+    
+      if (err) throw err;
+    
+    });
+    
+    res.redirect("/review?asin=" + asin);
+  
+    //This code should be removed before submission, it is only here for posterity and in case something buggy happens.
+    /*sql = "SELECT * FROM reviews WHERE reviews.products_ASIN = " + asin;
+    pool.query(sql, function(err, rows, fields) {
+    
+      if (err) throw err;
+      console.log(rows);
+      if (rows == "") {
+      
+        res.render("review", {"rows" : "It looks like there are no reviews for this product yet.", "id" : id, "asin" : asin});
+      
+      }
+    
+      else {
+      
+        res.render("review", {"rows" : rows, "id" : id, "asin" : asin});
+      
+      }
+    
+    });*/
+  
+  }
+  
+  else {
+    
+    res.render("login");
+    
+  }
+  
+});
+
+app.get("/newReview", function(req, res) {
+  
+  //Debug
+  console.log("User logged in is " + req.session.userLogged);
+  console.log("Is authenticated is " + (typeof(req.session.userLogged) != "undefined"));
+  
+  //isAuthenticated does not halt function() here, so we must manually check.
+  if (typeof(req.session.userLogged) != "undefined") {
+  
+    console.log(req.query.id);
+    console.log(req.query.asin);
+    let id = "";
+    if (req.query.id) {
+     
+        id = req.query.id;
+     
+    }
+   
+    if (id == "") {
+     
+        //or other dummy value
+        id = -1;
+     
+    }
+    
+    let asin = "";
+    if (req.param('asin')) {
+    
+      asin = req.param('asin');
+    
+    }
+  
+    if (asin == "") {
+     
+      console.log("here");
+      //Don't actually return, the page will be stuck in limbo. Just render the previous page.
+      return;
+     
+    }
+  
+    console.log("Attempted to leave review! Text is " + req.query.itemReview);
+  
+    //Needs to be updated to use userid.
+    let sql = "INSERT INTO reviews (idreviews, helpfulVotes, unhelpfulVotes, reviewText, products_productID, users_userID, products_ASIN) VALUES (DEFAULT, 0, 0, '" + req.query.itemReview + "', " + id + ", 0, '" + asin + "')";
+    pool.query(sql, function(err) {
+    
+      if (err) throw err;
+    
+    });
+    
+    res.redirect("/review?asin=" + asin);
+  
+  }
+  
+  else {
+    
+    res.render("login");
+    
+  }
+  
+  
+});
+
 app.get('/review', function (req, res) {
   
-  let title = "";
-  if (req.query.title) {
+  let id = "";
+  if (req.query.id) {
      
-      title = req.query.title;
+      id = req.query.id;
      
   }
    
-  if (title == "") {
+  if (id == "") {
      
       //or other dummy value
-      title = "Could not find title";
+      id = -1;
      
   }
   
-  let sql = "SELECT * FROM reviews WHERE reviews.products_productID IN (SELECT id FROM products WHERE products.title = '" + title + "')";
+  let asin = "";
+  if (req.param('asin')) {
+    
+    asin = req.param('asin');
+    
+  }
+  
+  if (asin == "") {
+     
+      //Don't actually return, the page will be stuck in limbo. Just render the previous page.
+      return;
+     
+  }
+  
+  console.log("id is " + id + " and asin is " + asin);
+  
+  let sql = "SELECT * FROM users RIGHT OUTER JOIN reviews ON users.userID = reviews.users_userID WHERE reviews.products_ASIN = '" + asin + "';"
   pool.query(sql, function(err, rows, fields) {
     
     if (err) throw err;
     console.log(rows);
-    if (rows == "") {
+    //Log whether something was returned or not.
+    console.log(Object.keys(rows).length === 0);
+    if (Object.keys(rows).length === 0) {
       
-      res.render("review", {"rows" : "It looks like there are no reviews for this product yet.", "title" : title});
+      res.render("review", {"rows" : "It looks like there are no reviews for this product yet.", "id" : id, "asin" : asin});
       
     }
     
     else {
       
-      res.render("review", {"rows" : rows, "title" : title});
+      res.render("review", {"rows" : rows, "id" : id, "asin" : asin});
       
     }
     
   });
-  
-  //displays title
-  /*let title = "";
-  if (req.query.title) {
-     
-      title = req.query.title;
-     
-  }
-   
-  if (title == "") {
-     
-      //or other dummy value
-      title = "Could not find title";
-     
-  }
-  
-  res.render("review", {"title" : title});*/
     
 });
 
-app.get("/search", async function(req, res) {
+//////////////// Products code //////////////////
+
+app.get("/search", isAuthenticated, async function(req, res) {
+    
     let keyword = "";
     if (req.query.keyword) {
         keyword = req.query.keyword;
@@ -91,7 +340,7 @@ app.get("/search", async function(req, res) {
     
     let keywordEncoded = encodeURIComponent(keyword.trim());
     
-    let apiUrl = "https://amazon-product-reviews-keywords.p.rapidapi.com/product/search?keyword=" + req.query.classSelected + "%20" + keywordEncoded + "&category=aps&country=US";
+    let apiUrl = "https://amazon-product-reviews-keywords.p.rapidapi.com/product/search?keyword=outdoor" + "%20" + keywordEncoded + "&category=aps&country=US";
     
     let response = await fetch(apiUrl, {
   	  "method": "GET",
@@ -116,13 +365,11 @@ app.get("/search", async function(req, res) {
     
     for (let k= 0; k < numProducts; k++) {
       i = Math.floor(Math.random() * (data.products.length - 1) + 1);
-      console.log("INDEX: " + i);
       try {
         sql = "INSERT IGNORE INTO products (asin, imageUrl, keyword, title, price, url) VALUES (?,?,?,?,?,?)";
         sqlParams = [data.products[i].asin, data.products[i].thumbnail, keyword, data.products[i].title, data.products[i].price.current_price, data.products[i].url];
         pool.query(sql, sqlParams, function (err, rows, fields) {
           if (err) throw err;
-          console.log(rows);
         });
       }
       catch(err) {
@@ -133,92 +380,226 @@ app.get("/search", async function(req, res) {
     res.render("results", {"keyword":keyword});
 }); 
 
-//this whole function needs to be redirected to a different database for user data
-app.get("/api/updateFavorite", function(req, res){ 
-  let sql;
-  let sqlParams;
-  switch (req.query.action) {
-    case "favorite": sql = "UPDATE products SET favorite = 1 WHERE asin = ?";
-                sqlParams = [req.query.asin];
-                break;
-    case "unfavorite": sql = "UPDATE products SET favorite = 0 WHERE asin = ?";
-                sqlParams = [req.query.asin];
-                break;
-  }//switch
-  pool.query(sql, sqlParams, function (err, rows, fields) {
-    if (err) throw err;
-    console.log(rows.affectedRows);
-  });
-    
-});//api/updateFavorite
-
-app.get("/api/checkFavorite", function(req, res){ 
-  let sql;
-  let sqlParams;
-  sql = "SELECT favorite FROM products WHERE asin = ?";
-  sqlParams = [req.query.asin];
-  pool.query(sql, sqlParams, function (err, rows, fields) {
-    if (err) throw err;
-    console.log("ROWS: " + rows);
-    res.send(rows[0].favorite);
-  });
-});//api/checkFavorite
-
 app.get("/api/updateDatabase", function(req, res){
   let sql;
   let sqlParams;
   switch (req.query.action) {
-    case "add": sql = "INSERT INTO products (imageUrl, keyword, title, price, url, asin) VALUES (?,?,?,?,?,?)";
-                sqlParams = [req.query.imageUrl, req.query.keyword, req.query.title, req.query.price, req.query.url, req.query.asin];
+    case "add": sql = "INSERT IGNORE INTO products (asin, imageUrl, keyword, title, price, url) VALUES (?,?,?,?,?,?)";
+                sqlParams = [req.query.asin, req.query.keyword, req.query.title, req.query.price, req.query.url, req.query.asin];
                 break;
-    case "delete": sql = "DELETE FROM products WHERE imageUrl = ?";
-                sqlParams = [req.query.imageUrl];
+    case "delete": sql = "DELETE FROM products WHERE asin = ?";
+                sqlParams = [req.query.asin];
                 break;
   }//switch
   pool.query(sql, sqlParams, function (err, rows, fields) {
     if (err) throw err;
-    console.log(rows);
     res.send(rows.affectedRows.toString());
   });
 });//api/updateDatabase
-
-app.get("/getKeywords",  function(req, res) {
-  let sql = "SELECT DISTINCT keyword FROM products WHERE favorite = ? ORDER BY keyword ";
-  let sqlParams = ['1'];
-  pool.query(sql, sqlParams, function (err, rows, fields) {
-     if (err) throw err;
-     console.log(rows);
-     res.render("favorites", {"rows":rows});
-  });  
-});//getKeywords
 
 //the main api for retrieving products from the database
 app.get("/api/getItems", function(req, res){
   let sql;
   let sqlParams;
+  
   switch (req.query.action) {
     case "asin": sql = "SELECT * FROM products WHERE asin = ? ORDER BY id DESC";
                 sqlParams = [req.query.asin];
                 break;
-    case "favorite": sql = "SELECT * FROM products WHERE favorite = ? ORDER BY id DESC";
-                sqlParams = ["1"];
-                break;
-    case "keywordAll": sql = "SELECT * FROM products WHERE keyword = ? ORDER BY id DESC";
+    case "keyword": sql = "SELECT * FROM products WHERE keyword = ? ORDER BY id DESC";
             sqlParams = [req.query.keyword];
-            break;
-    case "keywordFav": sql = "SELECT * FROM products WHERE keyword = ? AND favorite = ? ORDER BY id DESC";
-            sqlParams = [req.query.keyword, "1"];
             break;
   }
   pool.query(sql, sqlParams, function (err, rows, fields) {
     if (err) throw err;
-    console.log(rows);
-    res.send(rows);
+    
+    let htmlString = "";
+    
+    if (rows.length == 0) { 
+        htmlString = "<br><img class='noResultsImage' src='img/no_results.jpg'/><br>";
+    }
+
+    rows.forEach(function(row){
+      
+      htmlString += "<table id='productList'><tr><td id='imageCell'>";
+      htmlString += "<a href='"+ row.url + "'><img class='image' id='productImage' src='"+ row.imageUrl + "'/></a>";
+      htmlString += "</td><td id='productDetails'><div id='title'> "+ row.title + " </div>";
+      htmlString += "<hr>Price:&nbsp$"+ row.price + "<br>";
+      htmlString += "ASIN: "+ row.asin + "<br><br>";
+      
+      if (req.query.showReviewButton == "true") {
+          //I (James) added in this form that appears as a button which the user can click to leave review. It passes the item ASIN.
+          //Josh swapped out "row.asin.slice(0,-2)" for "row.asin" as the former was somehow chopping off the end of the asin.
+          htmlString += "<form id='goReview' action='/review'><input type='hidden' id='asin' name='asin' value='" + row.asin + "'/><button id='reviewButton' class='btn btn-primary'>See Reviews</button></form>";
+      }
+      
+      htmlString += "</td></tr></table>";
+    });
+    
+    let jsonString = {
+      "string" : htmlString,
+    };
+    
+    res.send(jsonString);
+
   });
+  
 });//api/getFavorites
 
+//////////////// Login Page code //////////////////
 
-//starting server
-app.listen(process.env.PORT, process.env.IP, function() {
-    console.log("Express server is running...");
+// use req.session.userLogged to find the currently logged in user
+// the middleware isAuthenticated (i.e. app.get("/page", isAuthenticated, function(req, res){}); ) can check if a user is logged in
+
+//NEW ROUTE STARTER BY JOSH - FEEL FREE TO DO WHATEVER YOU LIKE WITH IT
+app.get("/profile", function(req, res) {
+  
+  let username = 1; //need to replace this with whatever req.session value will give the current username logged in
+  let sql = "SELECT DISTINCT products_ASIN FROM reviews WHERE users_userID = ?";
+  let sqlParams = [username]; 
+  
+  console.log("username: " + username);
+
+  pool.query(sql, sqlParams,function(err, rows, fields) {
+  
+    if (err) throw err;
+    console.log(rows);
+    if (rows == "") {
+    
+      //Some other value may work.
+      res.render("profile", {"rows" : rows});
+    
+    }
+  
+    else {
+    
+      res.render("profile", {"rows" : rows});
+    
+    }
+  
+  });
+  
 });
+
+//when the user attempts to log in
+app.get("/login", function(req, res){
+    //uncomment the following three lines to have the user be auto logged in, to save time while testing
+    //req.session.userLogged = "test";
+    //res.redirect("/");
+    //return;
+  
+    res.render("login");
+});
+
+
+app.post("/loginPost", async function(req, res){
+    console.log(req.session.authenticated);
+    let username = req.body.username;
+    let password = req.body.password;
+    
+    let result = await checkUsername(username);
+    console.dir(result);
+    let hashedPwd = "";
+    
+    if (result.length > 0){
+        hashedPwd = result[0].password;
+    }else{
+        res.render("login", {"loginError":true});
+        return;
+    }
+    
+    let passwordMatch = await checkPassword(password, hashedPwd);
+    console.log("passwordMatch:" + passwordMatch);
+    
+    if (passwordMatch) {
+        req.session.userLogged = username;
+        req.session.save();
+        //console.log(req.session.authenticated);
+        
+        console.log("User logged in is " + req.session.userLogged);
+        console.log("Is authenticated is " + (typeof(req.session.userLogged) != "undefined"));
+        
+        res.redirect("/");
+    }else{
+        res.render("login", {"loginError":true});
+    }
+    //console.log(req.session.authenticated);
+});
+
+app.get("/signup", function(req, res){
+    res.render("signup");
+});
+
+//to create new accounts
+app.post("/makeNewAccount", async function(req, res){
+    let username = req.body.username;
+    let password = req.body.password;
+    if(username == "" || password == ""){
+        res.render("signup", {"emptyError":true});
+        return;
+    }
+    let result = await checkUsername(username);
+    console.dir(result);
+    if (result.length > 0){
+        res.render("signup", {"existsError":true});
+        return;
+    }
+    
+    let hashedPwd = await hashPassword(password);
+    
+    let sql = "INSERT INTO users (username, password) VALUES (?,?)";
+    let sqlParams = [username, hashedPwd];
+    
+    pool.query(sql, sqlParams, function (err, rows, fields) {
+        if (err) throw err;
+        console.log(rows);
+  });
+  res.render("login", {"accountSuccess":true});
+});
+
+//destroy the current session if the user logs out    
+app.get("/logout", function(req, res){
+    req.session.destroy();
+    res.redirect("/");
+})
+
+//verify that a username is in the database    
+function checkUsername(username){
+    let sql = "SELECT * FROM users WHERE username = ?";
+    return new Promise(function(resolve, reject){
+        pool.query(sql, [username], function (err, rows, fields){
+            if (err) throw err;
+            console.log("Rows found: " + rows.length);
+            resolve(rows);
+        });
+    });
+}
+
+//check to see if any user is logged on 
+function isAuthenticated(req, res, next) {
+    if (typeof(req.session.userLogged) == "undefined"){
+        res.redirect("/login");
+    }else{
+        next()
+    }
+}    
+
+//check a passord against the hash received from the database
+function checkPassword(password, hashedValue){
+    return new Promise( function(resolve, reject){
+        bcrypt.compare(password, hashedValue, function(err, result){
+            console.log("Result: " + result);
+            resolve(result);
+        })
+    })
+}
+
+//hash a password in order to add it to the database
+function hashPassword(password){
+    return new Promise(function(resolve, reject){
+        bcrypt.hash(password, saltRounds, function(err, hash) {
+            console.log("Hash: " + hash);
+            resolve(hash);
+        });
+    })
+}
--- public/js/functions.js
+++ public/js/functions.js
@@ -3,158 +3,53 @@
  
 $(document).ready(function(){
     
-    //Update the database if a favorite icon is clicked
-    $(document).on('click',".favoriteIcon",function() {
-    let asin = $(this).attr("id");
-
-        if ($(this).attr("src") == "img/favorite.png") {
-            $(this).attr("src", "img/favorite_on.png");
-            updateFavorite("favorite", asin);
-        }
-        else {
-            $(this).attr("src", "img/favorite.png");
-            updateFavorite("unfavorite", asin);
-        }
-        
-    });
-    
-    $("#classSelection").on("change", function() {
-        
-        let myClass = $("#classSelection").val();
-        $("#classSelected").val(myClass);
-        
-        console.log("CLASS SELECTED: " + myClass + " - stored " + $("#classSelected").val());
-    });
-    
-    $(".favoriteIcon").on("click", function() {
-        
-        let asin = $(this).attr("id");
-
-        if ($(this).attr("src") == "img/favorite.png") {
-            $(this).attr("src", "img/favorite_on.png");
-            updateFavorite("favorite", asin);
-        }
-        else {
-            $(this).attr("src", "img/favorite.png");
-            updateFavorite("unfavorite", asin);
-        }
-    });
-    
-    async function updateFavorite(action, asin) {
-        let url = `/api/updateFavorite?action=${action}&asin=${asin}`;
-        await fetch(url);
-    }
-    
     async function updateDatabase(action, imageUrl, keyword, title, price, url, asin) {
         let myUrl = `/api/updateDatabase?action=${action}&imageUrl=${imageUrl}&keyword=${keyword}&title=${title}&price=${price}&url=${url}&asin=${asin}`;
         await fetch(myUrl);
     }
     
-    //Populate all favorites when favorites page is loaded (page load clicks the keywordSelected button)
-    $("#keywordSelected").on("click", async function(){
-        let response = await  fetch(`/api/getItems?action=favorite`);
+    //Populate all results when results page is loaded (page load clicks the resultsLoader button)
+    $("#resultsLoader").on("click", async function(){
+        let keyword = $("#resultsLoader").attr('name');
+        
+        let response = await  fetch(`/api/getItems?action=keyword&keyword=${keyword}&showReviewButton=true`);
         let data = await response.json();
-     
-        $("#favorites").html("");
-        let htmlString = "";
-        let nextString = "";
-    
-        data.forEach(function(row){
-            
-           nextString = getProductTable(row).toString();
-           htmlString += nextString;
-        });
-
-        $("#favorites").append(htmlString);
+        let htmlString = data.string;
+        $("#resultscontainer").append(htmlString);
+        
     });
     
     //Populate all results when results page is loaded (page load clicks the resultsLoader button)
-    $("#resultsLoader").on("click", async function(){
-        //let keyword =  $(this).html().trim();
-        let keyword = $("#resultsLoader").attr('name');
+    $("#asin").on("click", async function(){
+        let asin = $("#asin").val();
         
-        let response = await  fetch(`/api/getItems?action=keywordAll&keyword=${keyword}`);
+        let response = await  fetch(`/api/getItems?action=asin&asin=${asin}&showReviewButton=false`);
         let data = await response.json();
-     
-        $("#resultscontainer").html("");
-        let htmlString = "";
-        let nextString = "";
-        
-        if (data.length == 0) { 
-            htmlString = "<br><img class='noResultsImage' src='img/no_results.jpg'/><br>";
-        }
-    
-        data.forEach(function(row){
-            
-           nextString = getProductTable(row);
-           htmlString += nextString;
-        });
-
-        $("#resultscontainer").append(htmlString);
-    });
-    
-    //Needs to be integrated with whichever database we are using, and cannot be tested.
-    $("#reviewsLoader").on("click", function() {
-    
-       // let response = all reviews of item from a route like /api/getReviews
-       // let data = await response.json();
-       // $("#reviewsContainer").html("");
-       // let htmlString = "";
-       // data.forEach(function(row) {
-           
-            //htmlString += row.{username field goes here}<br>;
-            //htmlString += row.{review text field goes here + "<br>";
-            //let favoritePercent = row.{helpful votes field}/(row.{helpful votes} + row.{unhelpful votes});
-            //htmlString += favoritePercent + " of adventurers found this review helpful";
-            //Insert code for vote helpful/unhelpful
-            
-       //});
+        let htmlString = data.string;
+        $("#reviewProductListing").append(htmlString);
         
     });
     
-    //filter favorites by keyword when one is clicked
-    $(".keywordLink").on("click", async function(){
-
-        let keyword =  $(this).html().trim();
-        $("#keywordSelected").val(keyword);
-        let response = await  fetch(`/api/getItems?action=keywordFav&keyword=${keyword}`);
-        let data = await response.json();
-     
-        $("#favorites").html("");
-        let htmlString = "";
-        let nextString = "";
-    
-        data.forEach(function(row){
-            
-           nextString = getProductTable(row);
-           htmlString += nextString;
-        });
+    //Used to list out all of the previously reviewed products.
+    $("#productsLoader").on("click", async function() {
         
-        $("#favorites").append(htmlString);
-    
-    });//keywordLink
-    
-
-    
-    //convert a row into a unified product listing HTML string
-    function getProductTable(row) {
+        let items = document.getElementsByClassName('listing');
+        let containers = document.getElementsByClassName('listingContainer');
+        console.log("Length: " + items.length);
+        for (var i = 0; i < items.length; i++) {
         
-        let htmlString = "";
-        htmlString += "<table id='productList'><tr><td id='imageCell'>";
-        if (row.favorite == "1")
-            htmlString += "<img class='favoriteIcon' src='img/favorite_on.png' id ='"+ row.asin + "' width='30'>";
-        else if (row.favorite =="0")
-            htmlString += "<img class='favoriteIcon' src='img/favorite.png' id ='"+ row.asin + "' width='30'>";
-        htmlString += "<a href='"+ row.url + "'><img class='image' id='productImage' src='"+ row.imageUrl + "'/></a>";
-        htmlString += "</td><td id='productDetails'><div id='title'> "+ row.title + " </div>";
-        htmlString += "<hr>Price:&nbsp$"+ row.price + "<br>";
-        htmlString += "ASIN: "+ row.asin + "<br><br></td></tr></table>";
+            items[i].click();
+            let asin = items[i].value;
         
-        //I (James) added in this form that appears as a button which the user can click to leave review. It only passes the name of the item.
-        htmlString += "<br><form id='goReview' action='/review'><input type='hidden' id='title' name='title' value='" + row.title + "'/><button>Leave Review</button></form>"
+            let response = await  fetch(`/api/getItems?action=asin&asin=${asin}&showReviewButton=true`);
+            let data = await response.json();
+            let htmlString = data.string;
+            containers[i].innerHTML = htmlString;
+            console.log(items[i]);
         
-        return htmlString;
-    }
+        } 
+        
+    });
 
 });
 
